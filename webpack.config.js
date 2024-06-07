474fe08385ed1e9b44eca75956c9f531140fbb40
ricardo vargas <104957708+ricardovargasri@users.noreply.github.com>
2024-06-06 22:41:39 -0500

    proyecto en proceso

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const path = require('path');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Ruta a tu archivo HTML
+      filename: './index.html' // Nombre del archivo HTML final
+      
+    })
+  ]
+};

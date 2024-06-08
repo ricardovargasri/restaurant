3e0dfdb9375278e6f458da2c0052529018b3a049
ricardo vargas <104957708+ricardovargasri@users.noreply.github.com>
2024-06-08 18:47:31 -0500

    prueba de funcionamiento .gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const path = require('path');
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
+    
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },

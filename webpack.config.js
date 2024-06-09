32feec6b73d87e64d23496e7fbb7fc7c7afcfee2
ricardo vargas <104957708+ricardovargasri@users.noreply.github.com>
2024-06-09 18:25:15 -0500

    pruebas en proceso

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,17 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const path = require('path');
 module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // Busca archivos con extensión .css
+        use: [
+          'style-loader', // Inyecta CSS en el HTML
+          'css-loader' // Procesa y carga CSS
+        ]
+      }
+    ]
+  },
   mode: 'development',
   entry: './src/index.js',
   output: {
